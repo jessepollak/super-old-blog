99dff905335380809adde41b7e497860d00f9808
Jesse Pollak <jpollak92@gmail.com>
2011-11-19 01:16:22 -0800

    added pg for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,7 +14,7 @@ JessepollakMe::Application.configure do
   # Compress JavaScripts and CSS
   config.assets.compress = true
   
-  config.assets.precompile = true
+  config.assets.precompile
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true

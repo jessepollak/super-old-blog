97aaa134d1927eb7ca83695feb1b032e5635fa6d
Jesse Pollak <jpollak92@gmail.com>
2011-12-08 15:21:07 -0800

    nowww

--- config/application.rb
+++ config/application.rb
@@ -44,8 +44,6 @@ module JessepollakMe
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-    
-    
-    config.middleware.use 'NoWWW'
+  
   end
 end

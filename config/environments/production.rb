fe9c777e3171463a0aece76ce0092b044761001c
Jesse Pollak <jpollak92@gmail.com>
2011-12-08 15:18:31 -0800

    nowww

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,5 +58,5 @@ JessepollakMe::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
   
-  config.middleware.use = 'NoWWW'
+  config.middleware.use 'NoWWW'
 end
